2b3e135425b45244492639fd748f56ce72fbcfe4
Sebastian soto <juancarloshj25@gmail.com>
2026-09-06 16:58:01 -0500

    encapsulo y creo mi constructor

--- libro.java
+++ libro.java
@@ -1,3 +1,18 @@
 public class libro {
-    
+    private String titulo;
+    private String autor;
+    private int ISBN;
+    private String categoria;
+    private int añoPublicacion;
+    private int copiasDisponibles;
+
+    public libro(String titulo, String autor, int ISBN, String categoria, int añoPublicacion , int copiasDisponibles) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ISBN = ISBN;
+        this.categoria = categoria;
+        this.añoPublicacion = añoPublicacion;
+        this.copiasDisponibles = copiasDisponibles;
+    }
+
 }
