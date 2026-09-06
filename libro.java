b0857b5fbee647557f0fc1e2700196c7c6b566c1
Sebastian soto <juancarloshj25@gmail.com>
2026-09-06 17:15:47 -0500

    empleo el get y set para poder recibir los datos y poder modificarlos junto con un metodo que devuelve el titulo, autor, ISBN que se planea utilizar mas adelante

--- libro.java
+++ libro.java
@@ -14,5 +14,46 @@ public class libro {
         this.añoPublicacion = añoPublicacion;
         this.copiasDisponibles = copiasDisponibles;
     }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public int getISBN() {
+        return ISBN;
+    }
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 
+    public int getAñoPublicacion() {
+        return añoPublicacion;
+    }
+
+    public void setAñoPublicacion(int añoPublicacion) {
+        this.añoPublicacion = añoPublicacion;
+    }
+    public int getCopiasDisponibles() {
+        return copiasDisponibles;
+    }
+    public void setCopiasDisponibles(int copiasDisponibles) {
+        this.copiasDisponibles = copiasDisponibles;
+    }
+
+    public String toString() {
+        return titulo + "-" + autor + "-" + ISBN;
+    }
 }
