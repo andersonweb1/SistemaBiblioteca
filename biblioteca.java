d530d0614209fe12401f0a16997e82d8b544f387
stiven <stivengaspar1095@gmail.com>
2026-09-21 10:29:37 -0500

    Se creo una clase publica biblioteca

---
+++ biblioteca.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class biblioteca {
+
+    private ArrayList<Libro>libros;
+
+    private HashMap<String, ArrayList<Libro>> librosPorAutor;
+
+}
