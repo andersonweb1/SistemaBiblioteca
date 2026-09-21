588ded10daf227329ee65d366cbf6dfe36c9c0c3
stiven <stivengaspar1095@gmail.com>
2026-09-21 10:48:32 -0500

    Se creo un agregar autor por libro

--- biblioteca.java
+++ biblioteca.java
@@ -7,4 +7,26 @@ public class biblioteca {
 
     private HashMap<String, ArrayList<Libro>> librosPorAutor;
 
+    public Biblioteca() {
+        libros = new ArrayList<>();
+        librosPorAutor = new HashMap<>();
+    }
+
+    // Agregar libro
+    public void agregarLibro(Libro libro) {
+
+        if (existeIsbn(libro.getIsbn())) {
+            throw new IllegalArgumentException(
+                    "Ya existe un libro con ese ISBN."
+            );
+        }
+
+        libro.add(libro);
+
+        String autor = libro.getAutor();
+
+        if (!librosPorAutor.containsKey(autor)) {
+            librosPorAutor.put(autor, new ArrayList<>());
+        }
+
 }
