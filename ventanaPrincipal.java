b3a45520ff7f237d6d156c63c54b213e2a6ff7ee
stiven <stivengaspar1095@gmail.com>
2026-09-22 10:58:57 -0500

    acomodo los botones ,el formulario, y el panel de busqueda

--- ventanaPrincipal.java
+++ ventanaPrincipal.java
@@ -37,9 +37,64 @@ public class VentanaPrincipal extends JFrame {
 
         JPanel panelPrincipal = new JPanel(new BorderLayout(10, 10));
 
-        JPanel panelFormulario = new JPanel(
-                new GridLayout(6, 2, 5, 5)
+        JPanel panelFormulario = new JPanel(new GridLayout(6, 2, 5, 5)
+
         );
 
+        txtTitulo = new JTextField();
+        txtAutor = new JTextField();
+        txtIsbn = new JTextField();
+        txtGenero = new JTextField();
+        txtAnio = new JTextField();
+        txtCopias = new JTextField();
 
+        panelFormulario.add(new JLabel("Título:"));
+        panelFormulario.add(txtTitulo);
+
+        panelFormulario.add(new JLabel("Autor:"));
+        panelFormulario.add(txtAutor);
+
+        panelFormulario.add(new JLabel("ISBN / Código:"));
+        panelFormulario.add(txtIsbn);
+
+        panelFormulario.add(new JLabel("Género:"));
+        panelFormulario.add(txtGenero);
+
+        panelFormulario.add(new JLabel("Año de publicación:"));
+        panelFormulario.add(txtAnio);
+
+        panelFormulario.add(new JLabel("Copias disponibles:"));
+        panelFormulario.add(txtCopias);
+
+        panelPrincipal.add(panelFormulario, BorderLayout.NORTH);
+
+        JPanel panelBotones = new JPanel();
+
+        btnAgregar = new JButton("Agregar libro");
+        btnEliminar = new JButton("Eliminar libro");
+        btnMostrarTodos = new JButton("Mostrar todos");
+
+        panelBotones.add(btnAgregar);
+        panelBotones.add(btnEliminar);
+        panelBotones.add(btnMostrarTodos);
+
+        panelPrincipal.add(panelBotones, BorderLayout.CENTER);
+
+        JPanel panelBusqueda = new JPanel();
+
+        txtBuscarAutor = new JTextField(20);
+        btnFiltrar = new JButton("Filtrar por autor");
+
+        panelBusqueda.add(new JLabel("Buscar autor:"));
+        panelBusqueda.add(txtBuscarAutor);
+        panelBusqueda.add(btnFiltrar);
+
+        panelPrincipal.add(panelBusqueda, BorderLayout.SOUTH);
+
+        crearTabla(panelPrincipal);
+
+        configurarEventos();
+
+        add(panelPrincipal);
+    }
 
